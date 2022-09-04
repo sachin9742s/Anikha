bf2f0e74607a0718159fe083df36708f3bc449aa
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2022-09-04 17:28:49 +0530

    Update __main__.py

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -297,7 +297,7 @@ def start(update: Update, context: CallbackContext):
             time.sleep(0.4)
             lol.delete()
             update.effective_message.reply_sticker(
-                "https://te.legra.ph/file/5a2c869ebb8479ac455b0.jpg"
+                "CAACAgQAAxkBAAJMO2MUktpccrva4ylR4mwnpPBwlpY3AAKCDQACaKOoULgJrOlkQfXnKQQ"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(
